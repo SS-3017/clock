72bbf60ab3daf1f94a3d00e8793d2ec7201b663a
SS-3017 <76947765+SS-3017@users.noreply.github.com>
2021-05-16 19:07:58 +0530

    Add files via upload
    commit

---
+++ sketch.js
@@ -0,0 +1,58 @@
+function setup() {
+  createCanvas(400, 400);
+  angleMode(DEGREES);
+}
+
+function draw() {
+  background(0);
+  translate(200, 200);
+  rotate(-90);
+  let hr = hour();
+  let mn = minute();
+  let sc = second();
+
+  strokeWeight(8);
+  stroke("Green");
+  noFill();
+  let secondAngle = map(sc, 0, 60, 0, 360);
+  arc(0, 0, 300, 300, 0, secondAngle);
+
+  stroke("Brown");
+  let minuteAngle = map(mn, 0, 60, 0, 360);
+  arc(0, 0, 280, 280, 0, minuteAngle);
+
+  stroke("Blue");
+  let hourAngle = map(hr % 12, 0, 12, 0, 360);
+  arc(0, 0, 260, 260, 0, hourAngle);
+
+  push();
+  rotate(secondAngle);
+  stroke("Green");
+  line(0, 0, 100, 0);
+  pop();
+
+  push();
+  rotate(minuteAngle);
+  stroke("Brown");
+  line(0, 0, 75, 0);
+  pop();
+
+  push();
+  rotate(hourAngle);
+  stroke("Blue");
+  line(0, 0, 50, 0);
+  pop();
+
+  stroke("Red");
+  point(0, 0);
+
+  push();
+  fill("Red");
+  noStroke();
+  rotate(90);
+  textSize(30);
+  textAlign(CENTER);
+  text(hr + ':' + mn + ':' + sc, 10, 190);
+  pop();
+
+  }
